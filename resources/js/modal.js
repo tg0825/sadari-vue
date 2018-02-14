4e1c1ae48d59170db090b5141be13d2de7766f20
tg0825 <tg0825@gmail.com>
2018-02-15 01:01:51 +0900

    모달 개선

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -10,6 +10,9 @@ var modal = (function (window, document, $, JQ) {
     var $modal = $('<div class="modal"></div>');
     var $modalMask = $('<div class="modal-mask"></div>');
     var $modalBox = $('<div class="modal-box"></div>');
+    var $modalHead = $('<div class="modal-head"></div>');
+    var $modalBody = $('<div class="modal-body"></div>');
+    var $modalClose = $('<button class="modal-close" type="button"><i class="fa fa-times" aria-hidden="true"></i></div>');
     var optionNumber = '<div class="option root">' +
         '<label class="option item">' +
         '<input type="checkbox" data-option="number">' +
@@ -17,18 +20,28 @@ var modal = (function (window, document, $, JQ) {
         '</div>';
 
     $modal.append($modalMask, $modalBox);
+    $modalBox.append($modalHead, $modalBody, $modalClose);
 
-    function open(content) {
-        close();
-        $modalBox.html(content + optionNumber);
+    function open(content, title) {
+        title = title || '결과';
+        $modalHead.empty().append(title, $(optionNumber));
+        $modalBody.empty().append(content);
         $modal.appendTo(JQ.BODY);
     }
 
     function close() {
-        $modal.attr('data-option','').remove();
+        if (confirm('결과창을 닫으시겠습니까?')) {
+            $modal.attr('data-option','').remove();
+        }
     }
 
-    JQ.DOC.on('click', '.modal-mask', close);
+    JQ.DOC
+        .on('click', '.modal-mask, .modal-close', close)
+        .on('keyup', function (e) {
+            if(JQ.DOC.find('.modal').length && e.which == 27) {
+                close();
+            }
+        });
 
     return {
         open: open,
