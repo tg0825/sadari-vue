2b823d7ac7e72f646a13b8c0784f2d1f08c5aa1a
tg0825 <tg0825@gmail.com>
2018-11-21 19:19:31 +0900

    파일 대문자로 변경

--- application/models/m_member.php
+++ application/models/m_member.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class m_member extends CI_Model {
+class M_member extends CI_Model {
     public function __construct()
     {
         parent::__construct();
