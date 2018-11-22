9359cc7c493613667655c35baa92f61bf52fbab0
tg0825 <tg0825@gmail.com>
2018-11-22 11:37:43 +0900

    되라

--- index.php
+++ index.php
@@ -53,8 +53,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	define('ENVIRONMENT', getenv("CLEARDB_DATABASE_URL") ? 'production' : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// define('ENVIRONMENT', getenv("CLEARDB_DATABASE_URL") ? 'production' : 'development');
 
 /*
  *---------------------------------------------------------------
