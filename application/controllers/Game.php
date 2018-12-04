21a96f706a6b891beac9dfe376d2c6222b4789e8
tg0825 <tg0825@gmail.com>
2018-12-04 14:02:56 +0900

    에러 표시 추가

--- application/controllers/Game.php
+++ application/controllers/Game.php
@@ -54,7 +54,7 @@ class game extends CI_Controller {
                 echo 'success';
             }
         } catch (Exception $e) {
-            echo 'error';
+            echo $e->getMessage();
         }
     }
     
