de007f0ffbd6b73fb9304a77976e4529116acdb0
tg0825 <tg0825@gmail.com>
2019-02-20 12:28:37 +0900

    아이콘 추가

--- application/views/game.php
+++ application/views/game.php
@@ -253,7 +253,7 @@ OneSignal.push(function() {
             
             <div class="option-btn-list">
                 <div>
-                    <label class="btn btn-s">
+                    <label class="btn btn-s btn-base">
                         <input
                             type="checkbox"
                             class="js-all-check-master"
@@ -265,6 +265,7 @@ OneSignal.push(function() {
                 </div>
                 <div>
                     <button type="button" class="js-split-view btn btn-s btn-base">
+                        <i class="va-m fa fa-list" aria-hidden="true"></i>
                         랜덤 대상자 확인
                     </button>
                 </div>
