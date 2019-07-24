c49dd85862e26c168a9d741123bbc750c756bd08
tg0825 <tg0825@gmail.com>
2019-07-24 13:03:37 +0900

    test

--- gulpfile.js
+++ gulpfile.js
@@ -42,32 +42,43 @@ var vendorJS = [
 
 // DEV TASKS /////////////////////////////////////////////////////////////////////////
 
+gulp.task('SCSS', function() {
+    return gulp.src(entryFile.scss);
+});
+
 // projectPath.dev경로 SCSS 처리
-gulp.task('devSCSS', function () {
-    return gulp.src(entryFile.scss)
+gulp.task('devSCSS', function() {
+    return gulp
+        .src(entryFile.scss)
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            errLogToConsole: false,
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
+        .pipe(
+            sass({
+                errLogToConsole: false,
+                outputStyle: 'expanded'
+            }).on('error', sass.logError)
+        )
         .pipe(rename(fileNames.cssFile + '.css'))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie 8', '> 5%'],
-            cascade: false
-        }))
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 versions', 'ie 8', '> 5%'],
+                cascade: false
+            })
+        )
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(projectPath.dev + '/css'))
         .pipe(bs.stream());
 });
 
 // projectPath.dev경로 JS 처리
-gulp.task('devJS', function () {
+gulp.task('devJS', function() {
     return gulp
         .src(clientJS)
         .pipe(sourcemaps.init())
-        .pipe(concat({
-            path: fileNames.jsFile + '.js'
-        }))
+        .pipe(
+            concat({
+                path: fileNames.jsFile + '.js'
+            })
+        )
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(projectPath.dev + '/js'))
         .pipe(bs.stream());
@@ -82,46 +93,48 @@ var spOpt = {
     distImg: projectPath.public + '/images',
     distCss: projectPath.public + '/css',
     bgPath: '../images/sp/',
-    addTheme: function (data) {
+    addTheme: function(data) {
         var info = data.spritesheet_info;
         var match = info.name.match(/hover--(\w+)/);
         data.theme = match && match[1];
     }
-}
+};
 
 // DEV 이미지 스프라이트 생성
-gulp.task('devSprite', function () {
+gulp.task('devSprite', function() {
     var util = spritesmith.util;
     var themeTemplate = util.createTemplate(
-        path.join(__dirname + '/resources/src/css', 'sp-template', 'sp-css.hbs'), [spOpt.addTheme, util.addPseudoClass]
+        path.join(
+            __dirname + '/resources/src/css',
+            'sp-template',
+            'sp-css.hbs'
+        ),
+        [spOpt.addTheme, util.addPseudoClass]
     );
 
     var opts = {
-        spritesmith: function (options, sprite, icons) {
+        spritesmith: function(options, sprite, icons) {
             options.retinaImgPath = spOpt.bgPath + options.imgName;
             options.imgPath = spOpt.bgPath + options.imgName;
             options.cssTemplate = themeTemplate;
             options.cssSpritesheetName = sprite;
             options.padding = 15;
             options.cssName = sprite + '.scss';
-            options.cssVarMap = function (sp) {
+            options.cssVarMap = function(sp) {
                 sp.name = sp.name;
             };
             return options;
         }
-    }
+    };
 
     var spriteData = gulp
         .src(spOpt.img)
         .pipe(spritesmith(opts))
-        .on('error', function (err) {
+        .on('error', function(err) {
             console.log(err);
         });
 
-    del([
-        spOpt.devCss + '/*',
-        spOpt.devImg + '/*',
-    ]);
+    del([spOpt.devCss + '/*', spOpt.devImg + '/*']);
     spriteData.img.pipe(gulp.dest(spOpt.devImg + '/sp'));
     spriteData.css.pipe(gulp.dest(spOpt.css));
 });
@@ -130,7 +143,7 @@ gulp.task('devSprite', function () {
  * browserSync
  * 1. php 실행
  */
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', function() {
     php.server({
         port: 8001
     });
@@ -140,7 +153,7 @@ gulp.task('browser-sync', function () {
         port: 8080,
         open: true,
         notify: false,
-        browser: ["google chrome"]
+        browser: ['google chrome']
     });
 });
 
@@ -150,16 +163,16 @@ gulp.task('browser-sync', function () {
  * 2. sass 실행
  * 3. css, js 파일 감시
  */
-gulp.task('default', ['browser-sync', 'devSCSS', 'devJS', 'devSprite'], function () {
-    gulp.watch(projectPath.src + '/**/*.scss', ['devSCSS']);
-    gulp.watch(projectPath.src + '/**/*.js', ['devJS']);
-    gulp.watch(['./**/*.php'], bs.reload);
-});
+gulp.task(
+    'default',
+    ['browser-sync', 'devSCSS', 'devJS', 'devSprite'],
+    function() {
+        gulp.watch(projectPath.src + '/**/*.scss', ['devSCSS']);
+        gulp.watch(projectPath.src + '/**/*.js', ['devJS']);
+        gulp.watch(['./**/*.php'], bs.reload);
+    }
+);
 
-gulp.task('bs', ['browser-sync'], function () {
-    gulp.watch([
-        './**/*.php',
-        './**/*.css',
-        './**/*.js'
-    ], bs.reload);
+gulp.task('bs', ['browser-sync'], function() {
+    gulp.watch(['./**/*.php', './**/*.css', './**/*.js'], bs.reload);
 });
