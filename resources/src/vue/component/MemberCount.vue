9df1b0e9eb7d3f25959d231fc7b0731bbdab6144
tg0825 <tg0825@gmail.com>
2019-08-23 00:55:44 +0900

    맴버 수 추가

---
+++ resources/src/vue/component/MemberCount.vue
@@ -0,0 +1,43 @@
+<template>
+    
+    <span class="member-list number">
+        {{maxCount}}
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'memberCount',
+    computed: {
+        maxCount() {
+            return this.$store.state.memberList.length
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .member-list.number {
+        margin-top: 8px;
+        margin-right: 10px;
+        float: left;
+        font-size: 20px;
+        color: #262626;
+        font-weight: normal;
+    }
+
+    .member-list.number:before {
+        font-size: 20px;
+        display: inline-block;
+        font-family: 'Font Awesome 5 Free';
+        content: '\f183';
+        vertical-align: baseline;
+        margin-right: 4px;
+        display: inline-block;
+        font-style: normal;
+        font-variant: normal;
+        text-rendering: auto;
+        -webkit-font-smoothing: antialiased;
+        font-weight: 900;
+    }
+</style>
